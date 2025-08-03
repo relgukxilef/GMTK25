f4b25a5038c1a6f75ec61601d815263bd294cb76
Felix Kugler <relgukxilef@gmail.com>
2025-08-03 14:42:21 +0200

    Replace timeline graphics

--- Assets/TimeLineSplit.cs
+++ Assets/TimeLineSplit.cs
@@ -2,17 +2,15 @@ using UnityEngine;
 
 public class TimeLineSplit : MonoBehaviour
 {
-    public SpriteRenderer top, bottom;
+    public SpriteRenderer top;
     public int height, width;
 
     // Start is called before the first frame update
     void Start()
     {
         top.transform.localPosition =
-            new Vector3(1, height + 0.5f, 2);
-        bottom.transform.localPosition =
-            new Vector3(2, - 0.5f, 2);
-        bottom.size = new Vector2(width - 1, height - 0.5f);
+            new Vector3(0.4f, height + 0.5f, 2);
+        top.size = new Vector2(2, height + 1);
     }
 
     // Update is called once per frame
